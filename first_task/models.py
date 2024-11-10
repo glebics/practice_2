c69b3f4c0ccee663c87565dd9e2e2794c52d2770
g.rahimzhanov <cubicscience@yandex.ru>
2024-11-10 17:08:00 +0300

    added comment/annotation for models

--- first_task/models.py
+++ first_task/models.py
@@ -6,9 +6,12 @@ from .database import BaseModel
 # Таблица жанров
 class Genre(BaseModel):
     __tablename__ = 'genre'
+    __table_args__ = {
+        "comment": "Таблица, содержащая информацию о жанрах книг"}
 
-    pk_genre_id = Column(Integer, primary_key=True)
-    name_genre = Column(String, nullable=False)
+    pk_genre_id = Column(Integer, primary_key=True,
+                         comment="Уникальный идентификатор жанра")
+    name_genre = Column(String, nullable=False, comment="Название жанра")
 
     # Связь с книгами
     books = relationship("Book", back_populates="genre")
@@ -17,9 +20,11 @@ class Genre(BaseModel):
 # Таблица авторов
 class Author(BaseModel):
     __tablename__ = 'author'
+    __table_args__ = {"comment": "Таблица, содержащая информацию об авторах"}
 
-    pk_author_id = Column(Integer, primary_key=True)
-    name_author = Column(String, nullable=False)
+    pk_author_id = Column(Integer, primary_key=True,
+                          comment="Уникальный идентификатор автора")
+    name_author = Column(String, nullable=False, comment="Имя автора")
 
     # Связь с книгами
     books = relationship("Book", back_populates="author")
@@ -28,10 +33,14 @@ class Author(BaseModel):
 # Таблица городов
 class City(BaseModel):
     __tablename__ = 'city'
+    __table_args__ = {
+        "comment": "Таблица, содержащая информацию о городах доставки"}
 
-    pk_city_id = Column(Integer, primary_key=True)
-    name_city = Column(String, nullable=False)
-    days_delivery = Column(Integer, nullable=False)
+    pk_city_id = Column(Integer, primary_key=True,
+                        comment="Уникальный идентификатор города")
+    name_city = Column(String, nullable=False, comment="Название города")
+    days_delivery = Column(Integer, nullable=False,
+                           comment="Количество дней для доставки в город")
 
     # Связь с клиентами
     clients = relationship("Client", back_populates="city")
@@ -40,11 +49,14 @@ class City(BaseModel):
 # Таблица клиентов
 class Client(BaseModel):
     __tablename__ = 'client'
+    __table_args__ = {"comment": "Таблица, содержащая информацию о клиентах"}
 
-    pk_client_id = Column(Integer, primary_key=True)
-    name_client = Column(String, nullable=False)
-    city_id = Column(Integer, ForeignKey('city.city_id'))
-    email = Column(String, nullable=False)
+    pk_client_id = Column(Integer, primary_key=True,
+                          comment="Уникальный идентификатор клиента")
+    name_client = Column(String, nullable=False, comment="Имя клиента")
+    city_id = Column(Integer, ForeignKey('city.pk_city_id'),
+                     comment="Идентификатор города проживания клиента")
+    email = Column(String, nullable=False, comment="Электронная почта клиента")
 
     # Связи с другими таблицами
     city = relationship("City", back_populates="clients")
@@ -54,13 +66,18 @@ class Client(BaseModel):
 # Таблица книг
 class Book(BaseModel):
     __tablename__ = 'book'
+    __table_args__ = {"comment": "Таблица, содержащая информацию о книгах"}
 
-    pk_book_id = Column(Integer, primary_key=True)
-    title = Column(String, nullable=False)
-    author_id = Column(Integer, ForeignKey('author.author_id'))
-    genre_id = Column(Integer, ForeignKey('genre.genre_id'))
-    price = Column(Float, nullable=False)
-    amount = Column(Integer, nullable=False)
+    pk_book_id = Column(Integer, primary_key=True,
+                        comment="Уникальный идентификатор книги")
+    title = Column(String, nullable=False, comment="Название книги")
+    author_id = Column(Integer, ForeignKey(
+        'author.pk_author_id'), comment="Идентификатор автора книги")
+    genre_id = Column(Integer, ForeignKey('genre.pk_genre_id'),
+                      comment="Идентификатор жанра книги")
+    price = Column(Float, nullable=False, comment="Цена книги")
+    amount = Column(Integer, nullable=False,
+                    comment="Количество экземпляров книги в наличии")
 
     # Связи с другими таблицами
     author = relationship("Author", back_populates="books")
@@ -71,10 +88,14 @@ class Book(BaseModel):
 # Таблица покупок
 class Buy(BaseModel):
     __tablename__ = 'buy'
+    __table_args__ = {
+        "comment": "Таблица, содержащая информацию о покупках клиентов"}
 
-    pk_buy_id = Column(Integer, primary_key=True)
-    buy_description = Column(String)
-    client_id = Column(Integer, ForeignKey('client.client_id'))
+    pk_buy_id = Column(Integer, primary_key=True,
+                       comment="Уникальный идентификатор покупки")
+    buy_description = Column(String, comment="Описание покупки")
+    client_id = Column(Integer, ForeignKey('client.pk_client_id'),
+                       comment="Идентификатор клиента, совершившего покупку")
 
     # Связи с другими таблицами
     client = relationship("Client", back_populates="buys")
@@ -85,9 +106,12 @@ class Buy(BaseModel):
 # Таблица этапов покупки
 class Step(BaseModel):
     __tablename__ = 'step'
+    __table_args__ = {
+        "comment": "Таблица, содержащая информацию о различных этапах покупки"}
 
-    pk_step_id = Column(Integer, primary_key=True)
-    name_step = Column(String, nullable=False)
+    pk_step_id = Column(Integer, primary_key=True,
+                        comment="Уникальный идентификатор этапа")
+    name_step = Column(String, nullable=False, comment="Название этапа")
 
     # Связь с таблицей buy_step
     buy_steps = relationship("BuyStep", back_populates="step")
@@ -96,11 +120,17 @@ class Step(BaseModel):
 # Таблица покупаемых книг
 class BuyBook(BaseModel):
     __tablename__ = 'buy_book'
+    __table_args__ = {
+        "comment": "Таблица, содержащая информацию о книгах, приобретенных в рамках покупок"}
 
-    pk_buy_book_id = Column(Integer, primary_key=True)
-    buy_id = Column(Integer, ForeignKey('buy.buy_id'))
-    book_id = Column(Integer, ForeignKey('book.book_id'))
-    amount = Column(Integer, nullable=False)
+    pk_buy_book_id = Column(Integer, primary_key=True,
+                            comment="Уникальный идентификатор записи о приобретенной книге")
+    buy_id = Column(Integer, ForeignKey('buy.pk_buy_id'),
+                    comment="Идентификатор покупки")
+    book_id = Column(Integer, ForeignKey('book.pk_book_id'),
+                     comment="Идентификатор книги")
+    amount = Column(Integer, nullable=False,
+                    comment="Количество экземпляров книги, приобретенных в рамках покупки")
 
     # Связи с другими таблицами
     buy = relationship("Buy", back_populates="buy_books")
@@ -110,12 +140,18 @@ class BuyBook(BaseModel):
 # Таблица шагов покупки
 class BuyStep(BaseModel):
     __tablename__ = 'buy_step'
+    __table_args__ = {
+        "comment": "Таблица, связывающая этапы покупки с конкретными покупками"}
 
-    pk_buy_step_id = Column(Integer, primary_key=True)
-    buy_id = Column(Integer, ForeignKey('buy.buy_id'))
-    step_id = Column(Integer, ForeignKey('step.step_id'))
-    date_step_beg = Column(DateTime, nullable=False)
-    date_step_end = Column(DateTime)
+    pk_buy_step_id = Column(Integer, primary_key=True,
+                            comment="Уникальный идентификатор шага в процессе покупки")
+    buy_id = Column(Integer, ForeignKey('buy.pk_buy_id'),
+                    comment="Идентификатор покупки")
+    step_id = Column(Integer, ForeignKey('step.pk_step_id'),
+                     comment="Идентификатор этапа покупки")
+    date_step_beg = Column(DateTime, nullable=False,
+                           comment="Дата начала этапа покупки")
+    date_step_end = Column(DateTime, comment="Дата окончания этапа покупки")
 
     # Связи с другими таблицами
     buy = relationship("Buy", back_populates="buy_steps")
--- second_task/models.py
+++ second_task/models.py
@@ -5,22 +5,29 @@ from database import Base
 
 class SpimexTradingResult(Base):
     __tablename__ = 'spimex_trading_results'
+    __table_args__ = {
+        "comment": "Таблица, содержащая результаты торгов СПбМТСБ (Санкт-Петербургской международной товарно-сырьевой биржи)"}
 
-    id = Column(Integer, primary_key=True, index=True)
-    # Основное поле, которое должно быть заполнено
-    exchange_product_id = Column(String, nullable=False)
+    pk_spimex_id = Column(Integer, primary_key=True, index=True,
+                comment="Уникальный идентификатор записи") # название иземенено на pk_spimex_id,
+                                                           # могут быть конфликты
+    exchange_product_id = Column(
+        String, nullable=False, comment="Идентификатор биржевого продукта")
     exchange_product_name = Column(
-        String, nullable=True)  # Сделано необязательным
-    oil_id = Column(String, nullable=True)  # Сделано необязательным
-    delivery_basis_id = Column(String, nullable=True)  # Сделано необязательным
+        String, nullable=True, comment="Название биржевого продукта")
+    oil_id = Column(String, nullable=True, comment="Идентификатор типа нефти")
+    delivery_basis_id = Column(
+        String, nullable=True, comment="Идентификатор условия доставки")
     delivery_basis_name = Column(
-        String, nullable=True)  # Сделано необязательным
-    delivery_type_id = Column(String, nullable=True)  # Сделано необязательным
-    volume = Column(Float, nullable=True)  # Сделано необязательным
-    total = Column(Float, nullable=True)  # Сделано необязательным
-    count = Column(Integer, nullable=True)  # Сделано необязательным
-    date = Column(DateTime, nullable=False,
-                  default=func.now())  # Дата обязательна
-    created_on = Column(DateTime, server_default=func.now())
-    updated_on = Column(DateTime, server_default=func.now(),
-                        onupdate=func.now())
+        String, nullable=True, comment="Название условия доставки")
+    delivery_type_id = Column(String, nullable=True,
+                              comment="Идентификатор типа доставки")
+    volume = Column(Float, nullable=True, comment="Объем торгового продукта")
+    total = Column(Float, nullable=True, comment="Общая стоимость сделки")
+    count = Column(Integer, nullable=True, comment="Количество сделок")
+    date = Column(DateTime, nullable=False, default=func.now(),
+                  comment="Дата проведения торгов")
+    created_on = Column(DateTime, server_default=func.now(),
+                        comment="Дата создания записи")
+    updated_on = Column(DateTime, server_default=func.now(
+    ), onupdate=func.now(), comment="Дата последнего обновления записи")
